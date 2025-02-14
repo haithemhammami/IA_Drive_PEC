cfb59d2cff4df3e25158bdd5fd86e639dd0b94f8
Berki Khalida <khalidaberki1@gmail.com>
2025-02-14 15:06:37 +0100

    eslint removed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Désactive ESLint pendant la build
+  },
   images: {
-    domains: ['img.youtube.com'],
+    domains: ["img.youtube.com"],
   },
 };
 
