9a7841b6b6253bcd1654670a05b98d5251b1c7f4
Haithem-Hammami <haithem192000@gmail.com>
2025-02-14 18:18:42 +0100

    fix error route.5

--- next.config.js
+++ next.config.js
@@ -1,12 +1,46 @@
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+//  reactStrictMode: true,
+//  eslint: {
+//    ignoreDuringBuilds: true, // Désactive ESLint pendant la build
+//  },
+//  images: {
+//    domains: ["img.youtube.com"],
+//  },
+//};
+//
+//module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
+const path = require("path")
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true, // Désactive ESLint pendant la build
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: ["img.youtube.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+        pathname: "/plus/img/logos/**",
+      },
+    ],
+    dangerouslyAllowSVG: true,
   },
-};
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /favicon\.ico$/,
+      use: path.resolve(__dirname, "node_modules/null-loader"),
+    })
+    return config
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "bcrypt"],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
