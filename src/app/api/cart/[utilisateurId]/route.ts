717365539a52c7859b77a694e6b8b5d4f3a01fa4
Haithem-Hammami <haithem192000@gmail.com>
2025-02-14 18:00:50 +0100

    fix error route.2

--- src/app/api/cart/[utilisateurId]/route.ts
+++ src/app/api/cart/[utilisateurId]/route.ts
@@ -1,46 +1,42 @@
-import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
-import jwt from "jsonwebtoken";
-import Stripe from "stripe";
+import { type NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/lib/prisma"
+import jwt from "jsonwebtoken"
+import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2025-01-27.acacia",
-});
+})
 
-type ContextType = {
-  params: { utilisateurId: string }
-}
-
-export async function GET(request: NextRequest, context: ContextType) {
-  const utilisateurIdInt = parseInt(context.params.utilisateurId);
+export async function GET(request: NextRequest, { params }: { params: { utilisateurId: string } }) {
+  const utilisateurIdInt = Number.parseInt(params.utilisateurId)
 
   if (isNaN(utilisateurIdInt)) {
-    return NextResponse.json({ message: "ID utilisateur invalide" }, { status: 400 });
+    return NextResponse.json({ message: "ID utilisateur invalide" }, { status: 400 })
   }
 
   // Récupérer le token JWT de l'utilisateur connecté
-  const token = request.headers.get('Authorization')?.split(' ')[1];
+  const token = request.headers.get("Authorization")?.split(" ")[1]
   if (!token) {
-    return NextResponse.json({ message: "Non authentifié" }, { status: 401 });
+    return NextResponse.json({ message: "Non authentifié" }, { status: 401 })
   }
 
   // Vérifier le token JWT
-  const secret = process.env.JWT_SECRET;
+  const secret = process.env.JWT_SECRET
   if (!secret) {
-    return NextResponse.json({ message: "JWT secret non défini" }, { status: 500 });
+    return NextResponse.json({ message: "JWT secret non défini" }, { status: 500 })
   }
 
-  let decoded: { userId: number };
+  let decoded: { userId: number }
   try {
-    decoded = jwt.verify(token, secret) as { userId: number };
+    decoded = jwt.verify(token, secret) as { userId: number }
   } catch (error: unknown) {
-    console.error('Erreur de vérification du token:', error instanceof Error ? error.message : 'Erreur inconnue');
-    return NextResponse.json({ message: "Token invalide ou expiré" }, { status: 401 });
+    console.error("Erreur de vérification du token:", error instanceof Error ? error.message : "Erreur inconnue")
+    return NextResponse.json({ message: "Token invalide ou expiré" }, { status: 401 })
   }
 
   // Vérifier si l'utilisateur connecté est le propriétaire du panier
   if (decoded.userId !== utilisateurIdInt) {
-    return NextResponse.json({ message: "Accès non autorisé" }, { status: 403 });
+    return NextResponse.json({ message: "Accès non autorisé" }, { status: 403 })
   }
 
   try {
@@ -48,10 +44,10 @@ export async function GET(request: NextRequest, context: ContextType) {
     const user = await prisma.utilisateur.findUnique({
       where: { id: utilisateurIdInt },
       select: { nom: true },
-    });
+    })
 
     if (!user) {
-      return NextResponse.json({ message: "Utilisateur non trouvé" }, { status: 404 });
+      return NextResponse.json({ message: "Utilisateur non trouvé" }, { status: 404 })
     }
 
     const cartItems = await prisma.cart.findMany({
@@ -61,50 +57,50 @@ export async function GET(request: NextRequest, context: ContextType) {
       include: {
         produit: true,
       },
-    });
+    })
 
     if (cartItems.length === 0) {
-      return NextResponse.json({ message: "Aucun produit dans le panier." }, { status: 404 });
+      return NextResponse.json({ message: "Aucun produit dans le panier." }, { status: 404 })
     }
 
-    const totalAmount = cartItems.reduce((total, item) => total + item.prix * item.quantite, 0);
+    const totalAmount = cartItems.reduce((total, item) => total + item.prix * item.quantite, 0)
 
-    return NextResponse.json({ cartItems, userName: user.nom, totalAmount }, { status: 200 });
+    return NextResponse.json({ cartItems, userName: user.nom, totalAmount }, { status: 200 })
   } catch (error: unknown) {
-    console.error("Erreur lors de la récupération du panier:", (error as Error).message);
-    return NextResponse.json({ message: "Erreur lors de la récupération du panier" }, { status: 500 });
+    console.error("Erreur lors de la récupération du panier:", (error as Error).message)
+    return NextResponse.json({ message: "Erreur lors de la récupération du panier" }, { status: 500 })
   }
 }
 
-export async function DELETE(request: NextRequest, context: ContextType) {
-  const utilisateurIdInt = parseInt(context.params.utilisateurId);
-  const { productId, removeAll } = await request.json();
-  const productIdInt = parseInt(productId);
+export async function DELETE(request: NextRequest, { params }: { params: { utilisateurId: string } }) {
+  const utilisateurIdInt = Number.parseInt(params.utilisateurId)
+  const { productId, removeAll } = await request.json()
+  const productIdInt = Number.parseInt(productId)
 
   if (isNaN(utilisateurIdInt) || isNaN(productIdInt)) {
-    return NextResponse.json({ message: "ID utilisateur ou produit invalide" }, { status: 400 });
+    return NextResponse.json({ message: "ID utilisateur ou produit invalide" }, { status: 400 })
   }
 
-  const token = request.headers.get('Authorization')?.split(' ')[1];
+  const token = request.headers.get("Authorization")?.split(" ")[1]
   if (!token) {
-    return NextResponse.json({ message: "Non authentifié" }, { status: 401 });
+    return NextResponse.json({ message: "Non authentifié" }, { status: 401 })
   }
 
-  const secret = process.env.JWT_SECRET;
+  const secret = process.env.JWT_SECRET
   if (!secret) {
-    return NextResponse.json({ message: "JWT secret non défini" }, { status: 500 });
+    return NextResponse.json({ message: "JWT secret non défini" }, { status: 500 })
   }
 
-  let decoded: { userId: number };
+  let decoded: { userId: number }
   try {
-    decoded = jwt.verify(token, secret) as { userId: number };
+    decoded = jwt.verify(token, secret) as { userId: number }
   } catch (error: unknown) {
-    console.error('Erreur de vérification du token:', error instanceof Error ? error.message : 'Erreur inconnue');
-    return NextResponse.json({ message: "Token invalide ou expiré" }, { status: 401 });
+    console.error("Erreur de vérification du token:", error instanceof Error ? error.message : "Erreur inconnue")
+    return NextResponse.json({ message: "Token invalide ou expiré" }, { status: 401 })
   }
 
   if (decoded.userId !== utilisateurIdInt) {
-    return NextResponse.json({ message: "Accès non autorisé" }, { status: 403 });
+    return NextResponse.json({ message: "Accès non autorisé" }, { status: 403 })
   }
 
   try {
@@ -113,57 +109,57 @@ export async function DELETE(request: NextRequest, context: ContextType) {
         utilisateurId: utilisateurIdInt,
         produitId: productIdInt,
       },
-    });
+    })
 
     if (!cartItem) {
-      return NextResponse.json({ message: "Produit non trouvé dans le panier." }, { status: 404 });
+      return NextResponse.json({ message: "Produit non trouvé dans le panier." }, { status: 404 })
     }
 
     if (removeAll || cartItem.quantite === 1) {
       await prisma.cart.delete({
         where: { id: cartItem.id },
-      });
+      })
     } else {
       await prisma.cart.update({
         where: { id: cartItem.id },
         data: { quantite: cartItem.quantite - 1 },
-      });
+      })
     }
 
-    return NextResponse.json({ message: "Produit mis à jour dans le panier." }, { status: 200 });
+    return NextResponse.json({ message: "Produit mis à jour dans le panier." }, { status: 200 })
   } catch (error: unknown) {
-    console.error("Erreur lors de la mise à jour du produit dans le panier:", (error as Error).message);
-    return NextResponse.json({ message: "Erreur lors de la mise à jour du produit dans le panier" }, { status: 500 });
+    console.error("Erreur lors de la mise à jour du produit dans le panier:", (error as Error).message)
+    return NextResponse.json({ message: "Erreur lors de la mise à jour du produit dans le panier" }, { status: 500 })
   }
 }
 
-export async function POST(request: NextRequest, context: ContextType) {
-  const utilisateurIdInt = parseInt(context.params.utilisateurId);
+export async function POST(request: NextRequest, { params }: { params: { utilisateurId: string } }) {
+  const utilisateurIdInt = Number.parseInt(params.utilisateurId)
 
   if (isNaN(utilisateurIdInt)) {
-    return NextResponse.json({ message: "ID utilisateur invalide" }, { status: 400 });
+    return NextResponse.json({ message: "ID utilisateur invalide" }, { status: 400 })
   }
 
-  const token = request.headers.get('Authorization')?.split(' ')[1];
+  const token = request.headers.get("Authorization")?.split(" ")[1]
   if (!token) {
-    return NextResponse.json({ message: "Non authentifié" }, { status: 401 });
+    return NextResponse.json({ message: "Non authentifié" }, { status: 401 })
   }
 
-  const secret = process.env.JWT_SECRET;
+  const secret = process.env.JWT_SECRET
   if (!secret) {
-    return NextResponse.json({ message: "JWT secret non défini" }, { status: 500 });
+    return NextResponse.json({ message: "JWT secret non défini" }, { status: 500 })
   }
 
-  let decoded: { userId: number };
+  let decoded: { userId: number }
   try {
-    decoded = jwt.verify(token, secret) as { userId: number };
+    decoded = jwt.verify(token, secret) as { userId: number }
   } catch (error: unknown) {
-    console.error('Erreur de vérification du token:', error instanceof Error ? error.message : 'Erreur inconnue');
-    return NextResponse.json({ message: "Token invalide ou expiré" }, { status: 401 });
+    console.error("Erreur de vérification du token:", error instanceof Error ? error.message : "Erreur inconnue")
+    return NextResponse.json({ message: "Token invalide ou expiré" }, { status: 401 })
   }
 
   if (decoded.userId !== utilisateurIdInt) {
-    return NextResponse.json({ message: "Accès non autorisé" }, { status: 403 });
+    return NextResponse.json({ message: "Accès non autorisé" }, { status: 403 })
   }
 
   try {
@@ -174,22 +170,22 @@ export async function POST(request: NextRequest, context: ContextType) {
       include: {
         produit: true,
       },
-    });
+    })
 
     if (cartItems.length === 0) {
-      return NextResponse.json({ message: "Aucun produit dans le panier." }, { status: 404 });
+      return NextResponse.json({ message: "Aucun produit dans le panier." }, { status: 404 })
     }
 
-    const totalAmount = cartItems.reduce((total, item) => total + item.prix * item.quantite, 0);
+    const totalAmount = cartItems.reduce((total, item) => total + item.prix * item.quantite, 0)
 
     let defaultStatus = await prisma.commandeStatut.findFirst({
       where: { statut: "En attente de paiement" },
-    });
+    })
 
     if (!defaultStatus) {
       defaultStatus = await prisma.commandeStatut.create({
         data: { statut: "En attente de paiement" },
-      });
+      })
     }
 
     const newOrder = await prisma.commande.create({
@@ -199,23 +195,23 @@ export async function POST(request: NextRequest, context: ContextType) {
         total: totalAmount,
         createdAt: new Date(),
         commandeDetails: {
-          create: cartItems.map(item => ({
+          create: cartItems.map((item) => ({
             produitId: item.produitId,
             quantite: item.quantite,
             prixUnitaire: item.prix,
           })),
         },
       },
-    });
+    })
 
-    const lineItems = cartItems.map(item => ({
+    const lineItems = cartItems.map((item) => ({
       price_data: {
         currency: "eur",
         product_data: { name: item.produit.nom },
         unit_amount: Math.round(item.prix * 100),
       },
       quantity: item.quantite,
-    }));
+    }))
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -227,15 +223,18 @@ export async function POST(request: NextRequest, context: ContextType) {
         orderId: newOrder.id.toString(),
         utilisateurId: utilisateurIdInt.toString(),
       },
-    });
+    })
 
-    return NextResponse.json({ 
-      message: "Commande créée avec succès.", 
-      order: newOrder, 
-      url: session.url 
-    }, { status: 201 });
+    return NextResponse.json(
+      {
+        message: "Commande créée avec succès.",
+        order: newOrder,
+        url: session.url,
+      },
+      { status: 201 },
+    )
   } catch (error: unknown) {
-    console.error("Erreur lors de la création de la commande:", (error as Error).message);
-    return NextResponse.json({ message: "Erreur lors de la création de la commande" }, { status: 500 });
+    console.error("Erreur lors de la création de la commande:", (error as Error).message)
+    return NextResponse.json({ message: "Erreur lors de la création de la commande" }, { status: 500 })
   }
 }
