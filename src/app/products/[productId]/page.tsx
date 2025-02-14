07b23e3d25758f839f9bf46999198d35e5f5aed2
Haithem-Hammami <haithem192000@gmail.com>
2025-02-15 00:52:39 +0100

    type props

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,21 +1,55 @@
+//import { prisma } from "@/lib/prisma";
+//import Image from "next/image";
+//import { Produit } from "@prisma/client";
+//import { GetServerSidePropsContext } from "next";
+//
+//export default async function ProductPage({params,}: GetServerSidePropsContext<{ productId: string }>) {
+//  // Vérification et conversion de l'ID du produit
+//  const productId = params ? Number(params.productId) : NaN;
+//  if (isNaN(productId)) {
+//    return <div className="text-center text-red-600 p-4">ID de produit invalide.</div>;
+//  }
+//
+//  // Récupérer le produit depuis la base de données
+//  const product = await prisma.produit.findUnique({
+//    where: { id: productId },
+//  });
+//
+//  if (!product) {
+//    return <div className="text-center text-red-600 p-4">Produit non trouvé.</div>;
+//  }
+//
+//  return (
+//    <div className="flex flex-col justify-center px-2 py-12 my-12 mx-auto w-3/4 aspect-square bg-white shadow-lg rounded-lg">
+//      {product.image && (
+//        <Image
+//          src={product.image}
+//          alt={product.nom}
+//          className="h-40 w-full object-cover rounded-md"
+//          width={300}
+//          height={300}
+//          priority
+//        />
+//      )}
+//      <h1 className="mt-4 text-base text-gray-700 font-bold">{product.nom}</h1>
+//      <p className="text-gray-600 mb-2">{product.description}</p>
+//      <p className="text-gray-600 font-medium text-lg">Prix : {product.prix} €</p>
+//    </div>
+//  );
+//}
+
 import { prisma } from "@/lib/prisma";
 import Image from "next/image";
-import { Produit } from "@prisma/client";
 
 
-type ProductPageProps = {
+type Props = {
   params: { productId: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-// Function pour récupérer les données
-async function getProduct(productId: number): Promise<Produit | null> {
-  return await prisma.produit.findUnique({
-    where: { id: productId },
-  });
-}
-
-export default async function ProductPage({ params }: ProductPageProps) {
+export default async function ProductPage({
+  params,
+}: Props) {
   // Vérification et conversion de l'ID du produit
   const productId = Number(params.productId);
   if (isNaN(productId)) {
@@ -23,7 +57,9 @@ export default async function ProductPage({ params }: ProductPageProps) {
   }
 
   // Récupérer le produit depuis la base de données
-  const product = await getProduct(productId);
+  const product = await prisma.produit.findUnique({
+    where: { id: productId },
+  });
 
   if (!product) {
     return <div className="text-center text-red-600 p-4">Produit non trouvé.</div>;
