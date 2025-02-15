0866de955a8fdc102d7c8e2eec9811173724ff8d
Haithem-Hammami <haithem192000@gmail.com>
2025-02-15 01:04:49 +0100

    type params

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,13 @@
 import { prisma } from "@/lib/prisma";
 import Image from "next/image";
-import { GetServerSidePropsContext } from "next";
 
-export default async function ProductPage({params,}: GetServerSidePropsContext<{ productId: string }>) {
+type Props = {
+  params: { productId: string };
+};
+
+export default async function ProductPage({ params }: Props) {
   // Vérification et conversion de l'ID du produit
-  const productId = params ? Number(params.productId) : NaN;
+  const productId = Number(params.productId);
   if (isNaN(productId)) {
     return <div className="text-center text-red-600 p-4">ID de produit invalide.</div>;
   }
@@ -22,7 +25,7 @@ export default async function ProductPage({params,}: GetServerSidePropsContext<{
     <div className="flex flex-col justify-center px-2 py-12 my-12 mx-auto w-3/4 aspect-square bg-white shadow-lg rounded-lg">
       {product.image && (
         <Image
-          src={product.image}
+          src={product.image || "/placeholder.svg"}
           alt={product.nom}
           className="h-40 w-full object-cover rounded-md"
           width={300}
@@ -36,49 +39,3 @@ export default async function ProductPage({params,}: GetServerSidePropsContext<{
     </div>
   );
 }
-
-//import { prisma } from "@/lib/prisma";
-//import Image from "next/image";
-//
-//
-//type Props = {
-//  params: { productId: string };
-//  searchParams: { [key: string]: string | string[] | undefined };
-//};
-//
-//export default async function ProductPage({
-//  params,
-//}: Props) {
-//  // Vérification et conversion de l'ID du produit
-//  const productId = Number(params.productId);
-//  if (isNaN(productId)) {
-//    return <div className="text-center text-red-600 p-4">ID de produit invalide.</div>;
-//  }
-//
-//  // Récupérer le produit depuis la base de données
-//  const product = await prisma.produit.findUnique({
-//    where: { id: productId },
-//  });
-//
-//  if (!product) {
-//    return <div className="text-center text-red-600 p-4">Produit non trouvé.</div>;
-//  }
-//
-//  return (
-//    <div className="flex flex-col justify-center px-2 py-12 my-12 mx-auto w-3/4 aspect-square bg-white shadow-lg rounded-lg">
-//      {product.image && (
-//        <Image
-//          src={product.image}
-//          alt={product.nom}
-//          className="h-40 w-full object-cover rounded-md"
-//          width={300}
-//          height={300}
-//          priority
-//        />
-//      )}
-//      <h1 className="mt-4 text-base text-gray-700 font-bold">{product.nom}</h1>
-//      <p className="text-gray-600 mb-2">{product.description}</p>
-//      <p className="text-gray-600 font-medium text-lg">Prix : {product.prix} €</p>
-//    </div>
-//  );
-//}
