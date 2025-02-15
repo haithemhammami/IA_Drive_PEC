e3331de944b71ba79c77c6cf0c4cafd04ab1721e
Haithem-Hammami <haithem192000@gmail.com>
2025-02-15 03:22:05 +0100

    modif Ai page

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -691,7 +691,7 @@ const SidebarMenuSub = React.forwardRef<
     className={cn(
       "mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l border-sidebar-border px-2.5 py-0.5",
       "group-data-[collapsible=icon]:hidden",
-      className
+      className ?? ""
     )}
     {...props}
   />
