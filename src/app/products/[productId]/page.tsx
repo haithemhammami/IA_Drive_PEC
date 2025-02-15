16de5928f5901572b3dc2f447d8b3dd127861eb6
Haithem-Hammami <haithem192000@gmail.com>
2025-02-15 02:36:40 +0100

    merge

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,21 +1,18 @@
+"use client";
 import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
-type PageProps = {
-  params: {
-    productId: string;
-  };
-};
+interface ProductPageProps {}
 
-export default async function ProductPage({ params }: PageProps) {
-  // Vérification et conversion de l'ID du produit
-  const productId = Number(params.productId);
+export default async function ProductPage({}: ProductPageProps) {
+  const searchParams = useSearchParams();
+  const productId = searchParams ? parseInt(searchParams.get('productId') || '', 10) : NaN;
+
   if (isNaN(productId)) {
-    return <div className="text-center text-red-600 p-4">ID de produit invalide.</div>;
+    return <div className="text-center text-red-600 p-4">Produit non trouvé.</div>;
   }
 
-  // Récupérer le produit depuis la base de données
   const product = await prisma.produit.findUnique({
     where: { id: productId },
   });
@@ -28,17 +25,15 @@ export default async function ProductPage({ params }: PageProps) {
     <div className="flex flex-col justify-center px-2 py-12 my-12 mx-auto w-3/4 aspect-square bg-white shadow-lg rounded-lg">
       {product.image && (
         <Image
-          src={product.image || "/file.svg"}
+          src={product.image}
           alt={product.nom}
           className="h-40 w-full object-cover rounded-md"
-          width={300}
-          height={300}
-          priority
         />
       )}
       <h1 className="mt-4 text-base text-gray-700 font-bold">{product.nom}</h1>
       <p className="text-gray-600 mb-2">{product.description}</p>
       <p className="text-gray-600 font-medium text-lg">Prix : {product.prix} €</p>
+      {/* Le bouton Ajouter au panier devra être géré côté client */}
     </div>
   );
 }
