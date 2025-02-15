d5c3f8aa930eb1ce869893f4df33cd78c4719ac3
haithemhammami <148556823+haithemhammami@users.noreply.github.com>
2025-02-15 03:31:06 +0100

    Update slider.tsx

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -13,11 +13,7 @@ const Slider = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex w-full touch-none select-none items-center",
-<<<<<<< HEAD
-      className  ??  ""
-=======
       className ?? ""
->>>>>>> main
     )}
     {...props}
   >
